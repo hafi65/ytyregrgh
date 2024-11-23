ac9e4260065d82d2c2d4656c3e667c9dd1ff6057
hafi65 <hafizalabiaamaan@gmail.com>
2024-11-23 17:18:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function myfunction(){
+    var t = 56;
+    var a = 57
+    document.getElementById("demo").innerHTML
+    = a*t;
+}
+
+function myBOOK(){
+    var T = 56;
+    var y = 6000
+    document.getElementById("demo").innerHTML
+    = y%T;
+}
+
+
